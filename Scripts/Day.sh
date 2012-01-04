bb02e12469f79ce508213b896ab6c9329570acd5
w.tknv <rreedd555@gmail.com>
2012-01-05 02:37:27 +0700

    i

--- Scripts/Day.sh
+++ Scripts/Day.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # sed -i 's/eclipse/solarized/g' ${HOME}/.vimrc
 # sed -i 's/75/90/g' ${HOME}/.bashrc
-sed -i 's/#color/color/g' ${HOME}/.muttrc
-xrdb -merge ${HOME}/.DayXresources
+# sed -i 's/#color/color/g' ${HOME}/.muttrc
+# xrdb -merge ${HOME}/.DayXresources
 cd ${HOME}/.surf
-ln -sf Day_style.css style.css
+# ln -sf Day_style.css style.css
 
 if [ ! "$(pidof tabbed)" ]
 then
 	tabbed -d > /tmp/tabbed.xid &
 fi
-
