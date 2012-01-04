27604b21920c966c22cabaa7de05d181e00c5392
w.tknv <rreedd555@gmail.com>
2012-01-05 02:36:58 +0700

    i

--- Scripts/DualHead.sh
+++ Scripts/DualHead.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output LVDS1 --primary --mode 1024x768
-xrandr --output VGA1 --above LVDS1 --mode 1280x1024 --noprimary
+xrandr --output LVDS1 --mode 1024x768
+xrandr --output VGA1 --right-of LVDS1 --mode 1280x1024
 
