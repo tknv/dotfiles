79b17aa2d449d61bf6bde7dd19d934357613f046
w.tknv <rreedd555@gmail.com>
2012-01-11 20:59:25 +0700

    add up interface

--- Scripts/wifi-auth-debug.sh
+++ Scripts/wifi-auth-debug.sh
@@ -2,6 +2,7 @@
 # This is for debugging, see log
 # usually in /etc/network/interfaces 
 # add wpa_supplicant file to wpa-conf section
+sudo ip link set wlan0 up
 sudo wpa_supplicant \
 -iwlan0 -c/home/tknv/iNetIDs/using \
 -Dwext -d
