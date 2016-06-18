d6d48467cb2f7e3f2f3bcd0639a467cb4cd745a5
w.tknv <rreedd555@gmail.com>
2016-06-18 23:00:36 +0800

    update, none-dbus

--- Scripts/DualHead.sh
+++ Scripts/DualHead.sh
@@ -2,4 +2,5 @@
 xrandr --output LVDS1 --mode 1280x800
 # xrandr --output VGA1 --below LVDS1 --mode 1280x1024 --rate 76
 # xrandr --output VGA1 --left-of LVDS1 --mode 1280x1024 --rate 76
-xrandr --output VGA1 --right-of LVDS1 --mode 1280x1024 --rate 76
+# xrandr --output VGA1 --right-of LVDS1 --mode 1280x1024 --rate 76
+xrandr --output VGA1 --right-of LVDS1 --mode 1366x768 --rate 59.79
