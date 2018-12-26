af5fba6a72497003fbb562f5ec38192ccdb598d0
Takanobu Watanabe <rreedd555@gmail.com>
2018-12-26 15:09:52 +0800

    update

--- Script/DualHead.sh
+++ Script/DualHead.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1920x1080 --rate 60.0
+xrandr --output eDP1 --mode 1600x900 --rate 60.00
 # xrandr --output LVDS1 --mode 1280x800
 # xrandr --output VGA1 --below LVDS1 --mode 1280x1024 --rate 76
 # xrandr --output VGA1 --left-of LVDS1 --mode 1280x1024 --rate 76
 # xrandr --output VGA1 --right-of LVDS1 --mode 1280x1024 --rate 76
 # xrandr --output VGA1 --right-of LVDS1 --mode 1366x768 --rate 59.79
-xrandr --output DVI-I-1 --left-of DP-1 --mode 1280x1024 --rate 60.0
+xrandr --output DP2 --right-of eDP1 --mode 1920x1080 --rate 60.00
 
 
 # Display port plug off/on then no initialize
